30acf7f6279b9656e4db2432d2360fbbec9813b8
Christoffer Ekeroth <ekeroth.christoffer@gmail.com>
2019-04-15 09:27:50 +0200

    Add "fenced_code" and "codehilite" extensions

--- generate.py
+++ generate.py
@@ -22,7 +22,7 @@ except FileExistsError:
 def generate_posts():
     metadata = []
     for post in os.listdir(os.path.join(BASE_DIR, 'posts')):
-        md = markdown.Markdown(extensions=['meta'])
+        md = markdown.Markdown(extensions=['meta', 'fenced_code', 'codehilite'])
         with open(os.path.join(BASE_DIR, 'posts', post), 'r') as f:
             post_html = md.convert(f.read())
         template = env.get_template('post.html')
