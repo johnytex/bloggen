fed83a376c79d0b697f2f264f20ea594d885e891
Christoffer Ekeroth <ekeroth.christoffer@gmail.com>
2019-04-15 09:04:39 +0200

    Add .html file extension to posts

--- generate.py
+++ generate.py
@@ -25,16 +25,18 @@ def generate_posts():
         md = markdown.Markdown(extensions=['meta'])
         with open(os.path.join(BASE_DIR, 'posts', post), 'r') as f:
             post_html = md.convert(f.read())
-        metadata.append(md.Meta)
         template = env.get_template('post.html')
         html = template.render(
             post_html=Markup(post_html)
         )
-        filename = os.path.join(
+        filename = os.path.splitext(os.path.basename(post))[0] + '.html'
+        md.Meta['filename'] = filename
+        metadata.append(md.Meta)
+        path = os.path.join(
             SITE_PATH,
-            os.path.splitext(os.path.basename(post))[0]
+            filename
         )
-        with open(filename, 'w') as f:
+        with open(path, 'w') as f:
             f.write(html)
     return metadata
 
